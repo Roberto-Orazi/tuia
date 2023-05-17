7ea1e5d5fdc87d5b8810beaedaa887cb4efed791
MatiiP <matiasprado18@gmail.com>
2023-05-17 18:05:52 -0300

    Terminamos la practica 3, solo me falto el ejercicio 6. Comenzamos con la practica 4.

--- Programacion/Matute/practica3.py
+++ Programacion/Matute/practica3.py
@@ -57,3 +57,186 @@ elif letraElegida == "e":
     print("La nota es: 10")
 else:
     print("Calificacion incorrecta")
+
+#6. Escribir un programa que le pregunte a las personas su fecha de cumpleanos y en base a eso imprimir su signo zodıaco. Recomendacion, pedirle a la persona ingresar la fecha con cierto formato, por ejemplo DD/MM y si la persona no lo respeta, imprimir un mensaje de error.
+
+
+
+
+
+#7. En el centro de la ciudad de Rosario el estacionamiento en vıa se encuentra tarifado y esta dividido en tres zonas con tarifas diferenciadas. El vehıculo puede estacionarse como maximo por 3 horas en el mismo sitio, luego de este tiempo, para renovar el servicio, hay que mover el vehıculo. La siguinte tabla muestra los costos por hora en cada una de las zonas:
+cospel = str(input("Ingrese la zona, ingresando: A, B o C ")).lower()
+if cospel == "a":
+    cantidad = int(input("Ingrese la cantidad de horas que quieres quedarte, maximo 3hs: "))
+    if cantidad == 1: 
+        print("Ustedes debe pagar $57,00")
+    elif cantidad == 2:
+        print("Usted debe abonar $71,20")
+    elif cantidad == 3:
+        print("Usted debe abonar $85,50")
+    else: 
+        print("Ingresaste mal la informacion por favor intente otra vez.")
+elif cospel == "b":
+    cantidad = int(input("Ingrese la cantidad de horas que quieres quedarte, maximo 3hs: "))
+    if cantidad == 1:
+        print("Usted debe abonar $47,00")
+    elif cantidad == 2:
+        print("Usted debe abonar $58,70")
+    elif cantidad == 3:
+        print("Usted debe abonar $70,50")
+    else: 
+        print("Ingresaste mal la informacion por favor intente otra vez.")
+elif cospel == "c":
+    cantidad = int(input("Ingrese la cantidad de horas que quieres quedarte, maximo 3hs: "))
+    if cantidad == 1:
+        print("Usted debe abonar $37,00")
+    elif cantidad == 2:
+        print("Usted debe abonar $46,70")
+    elif cantidad == 3:
+        print("Usted debe abonar $55,50")
+    else: 
+        print("Ingresaste mal la informacion por favor intente otra vez.")
+else: 
+    print("Ingresaste mal la informacion por favor intente otra vez.")
+
+
+
+# Una marca de ropa tienen descuentos diferentes dependiendo de la sede del local
+"""
+prenda = str(input("Ingrese que tipo de prenda quiere(zapatillas, remeras, pantalones): ")).lower()
+if prenda == "zapatillas":
+    ciudad = str(input("Ingrese de que ciudad sos(Rosario,Funes y Roldan): "))
+    if ciudad == "rosario":
+        print("El descuento es de 30%")
+    elif ciudad == "funes":
+        print("El descuento es del 40%")
+    elif ciudad == "roldan":
+        print("El descuento es de 25%")
+    else:
+        print("Ingresaste mal la informacion por favor intente devuelta.")
+elif prenda == "remeras":
+    ciudad = str(input("Ingrese de que ciudad sos(Rosario,Funes y Roldan): "))
+    if ciudad == "rosario":
+        print("El descuento es de 20%")
+    elif ciudad == "funes":
+        print("El descuento es del 30%")
+    elif ciudad == "roldan":
+        print("El descuento es de 15%")
+    else:
+        print("Ingresaste mal la informacion por favor intente devuelta.")
+elif prenda == "pantalones":
+    ciudad = str(input("Ingrese de que ciudad sos(Rosario,Funes y Roldan): "))
+    if ciudad == "rosario":
+        print("El descuento es de 10%")
+    elif ciudad == "funes":
+        print("El descuento es del 5%")
+    elif ciudad == "roldan":
+        print("El descuento es de 20%")
+    else:
+        print("Ingresaste mal la informacion por favor intente devuelta.")
+else:
+    print("Ingresaste mal la informacion por favor intente devuelta.")
+"""
+# . Ahora, supongamos que ademas dependiendo del dıa de la semana se puede recibir un descuento adicional acumulable. Es decir, si se recibio un descuento del 10 % segun el ıtem y la sede y la compra se realiza un lunes, el descuento total sera un 20 % . Escribir un programa en el que la persona pueda ingresar el d´ıa de la semana, el item a comprar y la sede del local. Luego, informar el descuento total a recibir. Utilizar la siguiente tabla de descuentos:
+
+dia = str(input("Ingrese el dia de la semana(lunes,martes,miercoles,jueves,viernes): ")).lower()
+if dia == "lunes":
+    prenda = str(input("Ingrese que prenda quiere comprar(zapatillas, remeras, pantalones): ")).lower()
+    if prenda == "zapatillas":
+        localidad = str(input("Ingrese de que localidad es(Rosario, Funes y Roldan): ")).lower()
+        if localidad == "rosario":
+            print("Su descuento por el dia y por el producto sera de %40.")
+        elif localidad == "funes":
+            print("Su descuento por el dia y por el producto es de 50%.")
+        elif localidad == "roldan":
+            print("Su descuento por el dia y por el producto es de 35%.")
+        else:
+            print("Ingresaste mal la informacion, intente otra vez.")
+    elif prenda == "remeras":
+        localidad = str(input("Ingrese de que localidad es(Rosario, Funes y Roldan): ")).lower()
+        if localidad == "rosario":
+            print("Su descuento por el dia y por el producto sera de 30%.")
+        elif localidad == "funes":
+            print("Su descuento por el dia y por el producto es de 40%.")
+        elif localidad == "roldan":
+            print("Su descuento por el dia y por el producto es de 25%.")
+        else:
+            print("Ingresaste mal la informacion, intente otra vez.")
+    elif prenda == "pantalones":
+        localidad = str(input("Ingrese de que localidad es(Rosario, Funes y Roldan): ")).lower()
+        if localidad == "rosario":
+            print("Su descuento por el dia y por el producto sera de 20%.")
+        elif localidad == "funes":
+            print("Su descuento por el dia y por el producto es de 15%.")
+        elif localidad == "roldan":
+            print("Su descuento por el dia y por el producto es de 30%.")
+        else:
+            print("Ingresaste mal la informacion, intente otra vez.")
+    else:
+        print("Ingresaste mal la informacion por favor intente devuelta")
+elif dia == "miercoles":
+    prenda = str(input("Ingrese que prenda quiere comprar(zapatillas, remeras, pantalones): ")).lower()
+    if prenda == "zapatillas":
+        localidad = str(input("Ingrese de que localidad es(Rosario, Funes y Roldan): ")).lower()
+        if localidad == "rosario":
+            print("Su descuento por el dia y por el producto sera de 28%.")
+        elif localidad == "funes":
+            print("Su descuento por el dia y por el producto es de 48%.")
+        elif localidad == "roldan":
+            print("Su descuento por el dia y por el producto es de 32%.")
+        else:
+            print("Ingresaste mal la informacion, intente otra vez.")
+    elif prenda == "remeras":
+        localidad = str(input("Ingrese de que localidad es(Rosario, Funes y Roldan): ")).lower()
+        if localidad == "rosario":
+            print("Su descuento por el dia y por el producto sera de 28%.")
+        elif localidad == "funes":
+            print("Su descuento por el dia y por el producto es de 38%.")
+        elif localidad == "roldan":
+            print("Su descuento por el dia y por el producto es de 22%.")
+        else:
+            print("Ingresaste mal la informacion, intente otra vez.")
+    elif prenda == "pantalones":
+        localidad = str(input("Ingrese de que localidad es(Rosario, Funes y Roldan): ")).lower()
+        if localidad == "rosario":
+            print("Su descuento por el dia y por el producto sera de 18%.")
+        elif localidad == "funes":
+            print("Su descuento por el dia y por el producto es de 13%.")
+        elif localidad == "roldan":
+            print("Su descuento por el dia y por el producto es de 28%.")
+        else:
+            print("Ingresaste mal la informacion, intente otra vez.")
+elif dia == "jueves":
+    prenda = str(input("Ingrese que prenda quiere comprar(zapatillas, remeras, pantalones): ")).lower()
+    if prenda == "zapatillas":
+        localidad = str(input("Ingrese de que localidad es(Rosario, Funes y Roldan): ")).lower()
+        if localidad == "rosario":
+            print("Su descuento por el dia y por el producto sera de 35%.")
+        elif localidad == "funes":
+            print("Su descuento por el dia y por el producto es de 45%.")
+        elif localidad == "roldan":
+            print("Su descuento por el dia y por el producto es de 30%.")
+        else:
+            print("Ingresaste mal la informacion, intente otra vez.")
+    elif prenda == "remeras":
+        localidad = str(input("Ingrese de que localidad es(Rosario, Funes y Roldan): ")).lower()
+        if localidad == "rosario":
+            print("Su descuento por el dia y por el producto sera de 25%.")
+        elif localidad == "funes":
+            print("Su descuento por el dia y por el producto es de 35%.")
+        elif localidad == "roldan":
+            print("Su descuento por el dia y por el producto es de 20%.")
+        else:
+            print("Ingresaste mal la informacion, intente otra vez.")
+    elif prenda == "pantalones":
+        localidad = str(input("Ingrese de que localidad es(Rosario, Funes y Roldan): ")).lower()
+        if localidad == "rosario":
+            print("Su descuento por el dia y por el producto sera de 15%.")
+        elif localidad == "funes":
+            print("Su descuento por el dia y por el producto es de 10%.")
+        elif localidad == "roldan":
+            print("Su descuento por el dia y por el producto es de 25%.")
+        else:
+            print("Ingresaste mal la informacion, intente otra vez.")
+else:
+    print("Ingresaste mal la informacion, intente otra vez.")
