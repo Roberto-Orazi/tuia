888b070cc84d8076b45cae99188b686106a22c86
Roberto-Orazi <robervgg@gmail.com>
2023-06-01 16:57:14 -0300

    Practicas de bash para el parcial

---
+++ Entorno/practicaBash/basico.sh
@@ -0,0 +1,50 @@
+#Si o si tiene que arrancar con la siguiente linea:
+#!bin/bash
+
+#los comentarios son como en python
+
+# Variables
+nombre="juan"
+edad=25
+#MUY IMPORTANTE QUE ESTE TODO JUNTO SIN ESPACIOS, EN DECLARACION DE VARIABLE
+#NO CAMBIA LAS COMILLAS DOBLES DE LAS SIMPLES PERO EN LOS ECHO SI
+#para acceder al valor de una variable se usa el signo dolar
+
+echo "hola, $nombre. Tenes $edad años"
+
+#para ejecutar el script se usa el comando bash
+#bash basico.sh
+
+#$0 es el nombre del script
+#$1 es el primer parametro
+#$2 es el segundo parametro
+#etc
+#$@ son todos los parametros pasados al script
+#como una lista de elementos separados por espacios
+#$* es lo mismo que $@ pero como un solo string
+#$# es la cantidad de parametros pasados al script
+
+echo "El script se llama: $0"
+echo "El primer argumento es: $1"
+echo "Todos los argumentos: $@"
+echo "Número total de argumentos: $#"
+
+#./basico.sh arg1 arg2 arg3
+
+#se obtiene lo siguiente
+#El script se llama: ./basico.sh
+#El primer argumento es: arg1
+#Todos los argumentos: arg1 arg2 arg3
+#Número total de argumentos: 3
+
+#COMANDO EXPR
+resultado=$(expr 2 + 2)
+echo "Suma: $resultado" #resultado es 4
+
+#DOBLE PARENTESIS
+resultado=$((10 - 2))
+echo "Resta: $resultado" # Resultado: 8
+
+#LET
+let resultado="4 * 3"
+echo "Multiplicación: $resultado" # Resultado: 12
---
+++ Entorno/practicaBash/if.sh
@@ -0,0 +1,90 @@
+#!/bin.bash
+edad=20
+
+if [$edad -eq 18]; then #eq es igual que
+    echo "Tienes 18 años"
+elif [$edad -gt 18]; then #gt es mayor que
+    echo "Eres mayor de edad"
+elif [$edad -lt 18]; then #lt es menor que
+    echo "Eres menor de edad"
+else                      # el ultimo elif esta demas pero es para explicar el lt
+    echo "No se que eres" #ACA IRIA ES MENOR DE EDAD PERO AGREGAMOS EL LT PARA EXPLICARLO
+fi                        #fin del if
+
+#COMPARACIONES
+# -eq (equal) es igual que
+# -ne (not equal) es distinto que
+# -gt (greater than) es mayor que
+# -lt (less than) es menor que
+# -ge (greater or equal) es mayor o igual que
+# -le (less or equal) es menor o igual que
+# -z (zero) es cadena vacia
+# -n (not zero) es cadena no vacia
+# = es igual que
+# != es distinto que
+# -e (exist) es archivo existe
+# -f (file) es archivo comun
+# -d (directory) es directorio
+# -r (read) es archivo tiene permiso de lectura
+# -w (write) es archivo tiene permiso de escritura
+# -x (execute) es archivo tiene permiso de ejecucion
+# -nt (newer than) es archivo1 mas nuevo que archivo2
+# -ot (older than) es archivo1 mas viejo que archivo2
+# -ef (equal file) es archivo1 es el mismo que archivo2
+
+#OPERADORES LOGICOS
+# && es and
+if [ $edad -gt 18 ] && [ $nombre = "Juan" ]; then
+    echo "La edad es mayor a 18 y el nombre es Juan."
+fi
+# || es or
+if [ $edad -eq 18 ] || [ $nombre = "Juan" ]; then
+    echo "La edad es igual a 18 o el nombre es Juan."
+fi
+# ! es not
+if ! [ $edad -gt 18 ]; then
+    echo "La edad no es mayor a 18."
+fi
+
+#COMPARADORES
+# == es igual
+if [ $edad == 18 ]; then
+    echo "La edad es igual a 18."
+fi
+# != es distinto
+if [ $edad != 18 ]; then
+    echo "La edad es distinta a 18."
+fi
+# < es menor que
+if [ $edad -lt 18 ]; then
+    echo "La edad es menor a 18."
+fi
+# > es mayor que
+if [ $edad -gt 18 ]; then
+    echo "La edad es mayor a 18."
+fi
+# <= es menor o igual que
+if [ $edad -le 18 ]; then
+    echo "La edad es menor o igual a 18."
+fi
+# >= es mayor o igual que
+if [ $edad -ge 18 ]; then
+    echo "La edad es mayor o igual a 18."
+fi
+
+#ANIDACIONES DE CONDICIONALES
+edad=25
+genero="masculino"
+
+if [$edad -ge 18]; then
+    echo "Eres mayor de edad"
+
+    if [$genero = "masculino"]; then
+        echo "Eres hombre y mayor de edad"
+    else
+        echo "Eres mujer y mayor de edad"
+    fi
+else
+    echo "Eres menor de edad"
+fi
+
