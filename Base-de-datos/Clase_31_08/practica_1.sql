608cede4d7460d7922e8da1864d8bfa3c1ea5dea
roberto <robervgg@gmail.com>
2023-09-04 20:27:56 -0300

    Add new folder

---
+++ Base-de-datos/Clase_31_08/practica_1.sql
@@ -0,0 +1,6 @@
+INSERT INTO dbo.Pacientes
+VALUES ('Laura', 'Montero', 30256321,1.67,63),
+('Micaela', 'Pensoli', 34589742,1.64,50),
+('Julieta', 'Rodriguez', 30256321,1.8,62),
+('Carlos', 'Rabin', 30256321,1.62,60),
+('Juan', 'Capristo', 30256321,1.65,69)
