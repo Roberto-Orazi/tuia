7a77b3f9bb0ff275dd4721940077c8b624991d6c
MatiiP <matiasprado18@gmail.com>
2023-10-19 11:04:49 -0300

    Update practica4.py

--- Programacion/Matute/practica4.py
+++ Programacion/Matute/practica4.py
@@ -129,6 +129,4 @@ while  altura_pila <= altura_monumento:
 print(dia)
 
 # 14. Escriba un programa reciba dos números como parámetros, y compute cuántos múltiplos del primero hay, que sean menores que el segundo.a. Implementarla utilizando un ciclo for, desde el primer número hasta el segundo. b. Implementarla utilizando un ciclo while, que multiplique el primer número hasta que sea mayor que el segundo. c. Comparar ambas implementaciones: ¿Cuál es más clara? ¿Cuál realiza menos operaciones?
-num1 = int(input("Ingrese el primer parametro: "))
-num2 = int(input("Ingrese el segundo parametro: "))
->>>>>>> Stashed changes
+
