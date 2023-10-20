2dae3a3cb342814966cf1956c25e7caff32d6702
Roberto-Orazi <robervgg@gmail.com>
2023-10-20 01:28:36 -0300

    add new project

---
+++ react-course/investment-calculator/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Expensas</h1>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
