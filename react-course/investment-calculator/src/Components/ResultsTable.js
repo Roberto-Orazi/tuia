3448eb5cc7a642814845be48ff1d45b19ad652f9
Roberto-Orazi <robervgg@gmail.com>
2023-10-21 01:45:36 -0300

    finish 115 chap

---
+++ react-course/investment-calculator/src/Components/ResultsTable.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+export const ResultsTable = () => {
+    return(
+        <table className="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Total Savings</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td>TOTAL SAVINGS END OF YEAR</td>
+            <td>INTEREST GAINED IN YEAR</td>
+            <td>TOTAL INTEREST GAINED</td>
+            <td>TOTAL INVESTED CAPITAL</td>
+          </tr>
+        </tbody>
+      </table>
+    )
+}
---
+++ react-course/investment-calculator/src/Components/UserInput.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+export const UserInput = () => {
+    const submitHandler = (event) => {
+        event.preventDefault()
+        console.log('Submit')
+    }
+
+    const resetHandler = () => {
+        console.log('reset')
+    }
+
+    const changeHandler = (input, value) => {
+        console.log(input, value)
+    }
+    return (
+        <form onSubmit={submitHandler} className="form">
+            <div className="input-group">
+                <p>
+                    <label htmlFor="current-savings">Current Savings ($)</label>
+                    <input onChange={(event) => changeHandler('current-savings', event.target.value)} type="number" id="current-savings" />
+                </p>
+                <p>
+                    <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+                    <input onChange={(event) =>
+                        changeHandler('yearly-contribution', event.target.value)
+                    }
+                        type="number"
+                        id="yearly-contribution" />
+                </p>
+            </div>
+            <div className="input-group">
+                <p>
+                    <label htmlFor="expected-return">
+                        Expected Interest (%, per year)
+                    </label>
+                    <input onChange={(event) =>
+                        changeHandler('expected-return', event.target.value)
+                    }
+                        type="number"
+                        id="expected-return" />
+                </p>
+                <p>
+                    <label htmlFor="duration">Investment Duration (years)</label>
+                    <input onChange={(event) =>
+                        changeHandler('duration', event.target.value)
+                    }
+                        type="number"
+                        id="duration" />
+                </p>
+            </div>
+            <p className="actions">
+                <button onClick={resetHandler} type="reset" className="buttonAlt">
+                    Reset
+                </button>
+                <button type="submit" className="button">
+                    Calculate
+                </button>
+            </p>
+        </form>
+    )
+}
