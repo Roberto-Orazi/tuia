f717559ceefac5c2019f11301b38d44943ac3c09
Roberto-Orazi <robervgg@gmail.com>
2023-10-23 01:32:12 -0300

    add new functions

--- react-course/investment-calculator/src/Components/UserInput.js
+++ react-course/investment-calculator/src/Components/UserInput.js
@@ -1,30 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const UserInput = () => {
+const initialValues = {
+    'current-savings': 1000,
+    'yearly-contribution': 1200,
+    'expected-return': 7,
+    'duration': 10,
+}
+
+export const UserInput = (props) => {
+    const [userInput, setUserInput] = useState(initialValues)
+
     const submitHandler = (event) => {
         event.preventDefault()
-        console.log('Submit')
+        props.onCalculate()
     }
 
     const resetHandler = () => {
-        console.log('reset')
+        setUserInput(initialValues)
     }
 
     const changeHandler = (input, value) => {
-        console.log(input, value)
+        setUserInput((prevInput) => {
+            return {
+                ...prevInput,
+                [input]: value,
+            }
+        })
     }
+
     return (
         <form onSubmit={submitHandler} className="form">
             <div className="input-group">
                 <p>
-                    <label htmlFor="current-savings">Current Savings ($)</label>
-                    <input onChange={(event) => changeHandler('current-savings', event.target.value)} type="number" id="current-savings" />
+                    <label
+                        htmlFor="current-savings">Current Savings ($)</label>
+                    <input
+                        onChange={(event) =>
+                            changeHandler('current-savings', event.target.value)}
+                        value={userInput['current-savings']}
+                        type="number"
+                        id="current-savings" />
                 </p>
                 <p>
-                    <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+                    <label
+                        htmlFor="yearly-contribution">Yearly Savings ($)</label>
                     <input onChange={(event) =>
                         changeHandler('yearly-contribution', event.target.value)
                     }
+                        value={userInput['yearly-contribution']}
                         type="number"
                         id="yearly-contribution" />
                 </p>
@@ -37,6 +60,7 @@ export const UserInput = () => {
                     <input onChange={(event) =>
                         changeHandler('expected-return', event.target.value)
                     }
+                        value={userInput['expected-return']}
                         type="number"
                         id="expected-return" />
                 </p>
@@ -45,6 +69,7 @@ export const UserInput = () => {
                     <input onChange={(event) =>
                         changeHandler('duration', event.target.value)
                     }
+                        value={userInput['duration']}
                         type="number"
                         id="duration" />
                 </p>
