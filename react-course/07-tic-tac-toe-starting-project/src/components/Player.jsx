a0310af4ac3a0576713ff9038f0cabb2aac7436f
Roberto-Orazi <robervgg@gmail.com>
2023-11-01 12:20:32 -0300

    add until ep79

---
+++ react-course/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react"
+
+export const Player = ({ initialName, symbol }) => {
+    const [playerName, setPlayerName] = useState(initialName)
+    const [isEditing, setIsEditing] = useState(false)
+
+    const editHandler = () => {
+        setIsEditing((editing) => !editing);
+    }
+    const handleChange = (event) => {
+        setPlayerName(event.target.value);
+    }
+    let editableName = <span className="player-name">{playerName}</span>
+    //let editButton = 'edit'
+    if (isEditing) {
+        editableName = <input type="text" required value={playerName} onChange={handleChange} />
+        //editButton = 'save'
+    }
+    return (
+        <li>
+            <span className="player">
+                {editableName}
+                <span className="player-symbol">
+                    {symbol}
+                </span>
+            </span>
+            <button onClick={editHandler}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
+    )
+}
