7b98f6ae12c40ea20f30ba7bcaad10cf1be5511c
roberto <robervgg@gmail.com>
2023-11-02 12:11:21 -0300

    finish ep 80

--- react-course/07-tic-tac-toe-starting-project/src/App.jsx
+++ react-course/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react"
+
+import { GameBoard } from "./components/GameBoard"
 import { Player } from "./components/Player"
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X')
+
+  const handleSelectPlayer(){
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
+  }
 
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player initialName='Player 1' symbol='X'/>
-          <Player initialName='Player 2' symbol='O'/>
+          <Player initialName='Player 1' symbol='X' />
+          <Player initialName='Player 2' symbol='O' />
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
       Log
     </main>
