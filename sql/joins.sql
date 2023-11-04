072f5e9ce59ede89ea43f4fdb7c312bdc7284243
Roberto-Orazi <robervgg@gmail.com>
2023-11-04 15:56:44 -0300

    add until union

--- sql/joins.sql
+++ sql/joins.sql
@@ -31,4 +31,31 @@ CREATE TABLE "Rewards" (
     "Reward" INTEGER,
     "Month" TEXT,
     PRIMARY KEY("RewardID", AUTOINCREMENT)
-)
+);
+
+INSERT INTO Rewards (EmployeeID,Reward,Month) VALUES
+
+(3, 200, 'January'),
+(2, 180, 'February'),
+(5, 200, 'March'),
+(1, 280, 'April'),
+(8, 160, 'May'),
+(null, null, 'June');
+
+SELECT FirstName,Reward, Month FROM Employees e
+INNER JOIN Rewards r ON e.EmployeeID = r.EmployeeID
+RIGHT JOIN Employees e ON e.EmployeeID = r.EmployeeID /*el right es lo contrario al left agarra la parte derecha de la interseccion + la interseccion de las tablas*/
+LEFT JOIN Rewards r ON e.EmployeeID = r.EmployeeID;
+/*El left join Nos devuelve la tabla de empleados + la interseccion con la tabla B que seria la de rewards y month
+Osea que pasa la tabla de firstName la devuelve completa por lo tanto los empleados que no tienen rewards van a aparecer null
+*/;
+
+SELECT FirstName,Reward, Month FROM Rewards r
+LEFT JOIN Employees e ON e.EmployeeID = r.EmployeeID;
+/*Algunos programas no nos dejan usar right join por lo cual invertimos las tablas y listo*/;
+
+/*FULL JOIN*/
+/*SERIA UNA UNION DE CONJUNTOS*/
+/*SIMULAMOS UN FULL JOIN UNIENDO UN LEFT JOIN CON UN RIGHT JOIN PASA QUE SI USAMOS UN UNION ALL SE REPITEN LOS DATOS YA QUE TENEMOS DATOS DUPLICADOS*/;
+
+
