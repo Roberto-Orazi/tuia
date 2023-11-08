ecb75eccc4d18d7396824e93f289d10863d11953
Roberto-Orazi <robervgg@gmail.com>
2023-11-08 10:47:02 -0300

    start new project and finish ep 173

--- react-course/management/src/App.jsx
+++ react-course/management/src/App.jsx
@@ -1,8 +1,45 @@
+import { useState } from "react";
+import { NoProjectsSelected } from "./components/NoProjectsSelected";
+import { SideBar } from "./components/SideBar";
+import { NewProject } from "./components/NewProject";
+
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+  const handleStartAddProject = () => {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    })
+  }
+  const handleAddProject = (projectData) => {
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+  console.log(projectsState)
+  let content
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject}/>
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectsSelected onStartAdd={handleStartAddProject} />
+  }
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <SideBar onStartAdd={handleStartAddProject} />
+      {content}
+    </main>
   );
 }
 
