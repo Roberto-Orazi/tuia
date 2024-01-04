84222d105bf749081bce399805819a8d6f874917
Roberto-Orazi <robervgg@gmail.com>
2024-01-04 09:36:56 -0300

    finish section 13

--- react-course/section-13/src/App.jsx
+++ react-course/section-13/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
 
   function handleSetCount(newCount) {
     setChosenCount(newCount)
+    setChosenCount((prevChosenCount) => prevChosenCount + 1)
   }
 
   return (
@@ -19,7 +20,7 @@ function App() {
       <Header />
       <main>
         <ConfigueCounter onset={handleSetCount} />
-        <Counter initialCount={chosenCount} />
+        <Counter key={chosenCount} initialCount={chosenCount} />
         <Counter initialCount={0} />
       </main >
     </>
