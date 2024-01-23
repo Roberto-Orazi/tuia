62311ed3c4ac3ffe8d20a876490e8a9212567523
Roberto-Orazi <robervgg@gmail.com>
2024-01-23 19:33:30 -0300

    Add, and finish part 1

---
+++ Js/parte-1/atributosElemento.js
@@ -0,0 +1,7 @@
+const elemento = document.querySelector('.miClaseCss')
+
+elemento.setAttribute('type', 'text') // por ejemplo nosotros ahora estamos cambiando el tipo de un input
+
+atributo = elemento.getAttribute('type')
+
+atributo = elemento.removeAttribute('type')
---
+++ Js/parte-1/seleccionElementos.js
@@ -0,0 +1,2 @@
+idPrueba = document.getElementById('Mi-id-de-mi-objeto')
+console.log(idPrueba) // Esto nos muestra que tipo de elemento es
