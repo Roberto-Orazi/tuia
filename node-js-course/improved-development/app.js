4eb487a5dccc5394dd7e8c2bc1cc842aba9a6549
Roberto-Orazi <robervgg@gmail.com>
2024-09-30 22:44:12 -0300

    add new exc & new course

--- node-js-course/improved-development/app.js
+++ node-js-course/improved-development/app.js
@@ -1,20 +1,18 @@
+const path = require('path')
 const express = require('express');
-
+const bodyParser = require('body-parser')
 const app = express();
 
-app.use('/',(req,res,next)=>{
-    console.log('This always run')
-    next()
+const adminRoutes = require('./routes/admin.js')
+const shopRoutes = require('./routes/shop.js')
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/admin',adminRoutes)
+app.use(shopRoutes)
+
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
-
-app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express add!</h1>');
-});
-
-app.use('/', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
-});
 
 app.listen(3000)
