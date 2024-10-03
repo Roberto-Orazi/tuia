513bd8aa2251cfeebcff28d5de694c715128acd0
roberto-orazi <robervgg@gmail.com>
2024-10-03 15:49:39 -0300

    add new exc

--- python/basic/funcion_suma.py
+++
@@ -1,2 +0,0 @@
-def sumValuesTriple(first_value: int, second_value: int, thrid_value: int) -> int:
-    print(first_value + second_value + thrid_value)
--- python/basic/funcion_suma_dos.py
+++
@@ -1,2 +0,0 @@
-def sumValues(first_value: int, second_value: int) -> int:
-    print(first_value + second_value)
